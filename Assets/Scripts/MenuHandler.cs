e2cb00c9df6238f2463e2c83fefa735ee7af0477
nashmia-riaz <nashmia-riaz@hotmail.com>
2020-08-13 19:48:02 +0100

    Added main menu

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
